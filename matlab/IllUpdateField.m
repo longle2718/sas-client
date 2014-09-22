9db205b1728004f5adf6a393b9b721ece6db5d61
Long Le <longle1@illinois.edu>
2014-09-22 00:02:42 -0500

    add IllUpdateField.m

---
+++ matlab/IllUpdateField.m
@@ -0,0 +1,17 @@
+% function resp = IllEventField(db, user, pwd, filename, op, field)
+% Apply operation 'op' with field 'field' on document 'filename'. 
+%
+% 'field' is json, i.e. has the form {<name>:<value>}.
+% 'op' includes (but not limited to, see the MongoDb field update operators
+% for the complete list): inc, max, min, mul, set, unset.
+%
+% Long Le
+% University of Illinois
+% longle1@illinois.edu
+%
+function resp = IllUpdateField(db, user, pwd, filename, op, field)
+
+params = {'dbname', db, 'colname', 'event', 'user', user, 'passwd', pwd};
+queryString = http_paramsToString(params);
+data = sprintf('{filename:"%s"}\n{$%s:%s}', filename, op, field);
+resp = urlread2(['https://acoustic.ifp.uiuc.edu:8081/write?' queryString], 'POST', data, [], 'READ_TIMEOUT', 10000);
