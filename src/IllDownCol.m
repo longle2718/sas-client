25c7b58d7a145909669d3d40e52eb6d3c6e7a7bf
Long Le <longle1@illinois.edu>
2015-12-02 10:16:57 +0700

    IllDownCol now use the GET /col interface

--- src/IllDownCol.m
+++ src/IllDownCol.m
@@ -9,5 +9,5 @@ function event = IllDownCol(servAddr, db, user, pwd, col, filename)
 
 params = {'dbname', db, 'colname', col, 'user', user, 'passwd', pwd};
 queryString = http_paramsToString(params);
-tmp = urlread2(['http://' servAddr ':8956/query?' queryString], 'POST', ['{"$and":[{"filename":"' filename '"}]}'], [], 'READ_TIMEOUT', 10000);
+tmp = urlread2(['http://' servAddr ':8956/col?' queryString], 'GET', ['{"$and":[{"filename":"' filename '"}]}'], [], 'READ_TIMEOUT', 10000);
 event = loadjson(tmp);
