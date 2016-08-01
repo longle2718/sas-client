36472b552a119aa57acb484b9522445b3cd804dd
Long Le <longle1@illinois.edu>
2016-08-01 12:00:13 -0500

    check for corrupted audio files

--- python/test/audio_proc_test.py
+++ python/test/audio_proc_test.py
@@ -29,8 +29,11 @@ events = IllQuery(servAddr,DB, USER, PWD, EVENT, q);
 print("Number of events found is "+str(len(events)))
 # bytes
 data = IllGridGet(servAddr, DB, USER, PWD, DATA, events[0]['filename'])
-with open('audio.wav', 'wb') as f:
-	f.write(data)
+if 'RIFF' == data[0:4].decode('utf-8'):
+	with open('audio.wav', 'wb') as f:
+		f.write(data)
+else:
+	sys.exit('Corrupted audio file!')
 
 fs,data = wavfile.read('audio.wav')
 T = np.arange(len(data))/fs
